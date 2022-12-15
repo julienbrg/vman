d7fccedd3753b7225d7a531e7d8b50d34a246e2e
julienbrg <julien@strat.cc>
2022-12-15 16:22:10 +0100

    add tag

--- contracts/Vman.sol
+++ contracts/Vman.sol
@@ -1,23 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.4;
+pragma solidity 0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Vman is Ownable {
-    
     string public cid;
+    string public tag;
 
     event Updated(
-        uint indexed timestamp, 
-        string indexed cid
-        );
+        uint indexed timestamp,
+        string indexed cid,
+        string indexed tag
+    );
 
-    constructor(string memory _cid) {
-        update(_cid);
+    constructor(string memory _cid, string memory _tag) {
+        update(_cid, _tag);
     }
 
-    function update(string memory _cid) public onlyOwner {
+    function update(string memory _cid, string memory _tag) public onlyOwner {
         cid = _cid;
-        emit Updated(block.timestamp, _cid); 
+        tag = _tag;
+        emit Updated(block.timestamp, _cid, _tag);
     }
 }
